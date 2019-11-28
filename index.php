c5d47889c019fa685691572317763d5244cb050c
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-11-28 09:33:49 +0000

    a

--- index.php
+++ index.php
@@ -1,220 +1,232 @@
+<?php
+
+if(isset($_GET['dev']) && $_GET['dev'] == 'yes'){
+  error_reporting(E_ALL);
+  ini_set('display_errors', 1);
+  ini_set('error_reporting', E_ALL);
+}
+
+date_default_timezone_set('UTC');
+
+session_start();
+
+// includes
+include('inc/db.php');
+include('inc/global_vars.php');
+include('inc/functions.php');
+
+// start timer for page loaded var
+$time = microtime();
+$time = explode(' ', $time);
+$time = $time[1] + $time[0];
+$start = $time;
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
 
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-  <title>UBLO Club</title>
+    <title><?php echo $global_vars['shop_title']; ?></title>
 
-  <!-- Bootstrap core CSS -->
-  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-  <!-- Custom styles for this template -->
-  <link href="css/shop-homepage.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="css/shop-homepage.css" rel="stylesheet">
 
 </head>
 
 <body>
 
-  <!-- Navigation -->
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="<?php echo $global_vars['shop_site_url']; ?>"><?php echo $global_vars['shop_title']; ?></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="index.php">Home
+                            <span class="sr-only">(current)</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Products</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- Page Content -->
     <div class="container">
-      <a class="navbar-brand" href="#">UBLO.Club</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="index.php">Home
-              <span class="sr-only">(current)</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Products</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">About</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Contact</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-
-  <!-- Page Content -->
-  <div class="container">
-
-    <div class="row">
-
-      <div class="col-lg-3">
-
-        <h1 class="my-4">UBLO Club</h1>
-        <div class="list-group">
-          <a href="#" class="list-group-item">Category 1</a>
-          <a href="#" class="list-group-item">Category 2</a>
-          <a href="#" class="list-group-item">Category 3</a>
-        </div>
-
-      </div>
-      <!-- /.col-lg-3 -->
-
-      <div class="col-lg-9">
-
-        <div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
-          <ol class="carousel-indicators">
-            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-          </ol>
-          <div class="carousel-inner" role="listbox">
-            <div class="carousel-item active">
-              <img class="d-block img-fluid" src="images/D_yJ8EJWkAM17Ua.jpeg" width="900" height="350" alt="First slide">
-            </div>
-            <div class="carousel-item">
-              <img class="d-block img-fluid" src="images/D_yJ8EJWkAM17Ua.jpeg" width="900" height="350" alt="Second slide">
-            </div>
-            <div class="carousel-item">
-              <img class="d-block img-fluid" src="images/D_yJ8EJWkAM17Ua.jpeg" width="900" height="350" alt="Third slide">
-            </div>
-          </div>
-          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-          </a>
-          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-          </a>
-        </div>
-
         <div class="row">
+            <div class="col-lg-3">
+                <h1 class="my-4">UBLO Club</h1>
+                <div class="list-group">
+                    <a href="#" class="list-group-item">Category 1</a>
+                    <a href="#" class="list-group-item">Category 2</a>
+                    <a href="#" class="list-group-item">Category 3</a>
+                </div>
 
-          <div class="col-lg-4 col-md-6 mb-4">
-            <div class="card h-100">
-              <a href="#"><img class="card-img-top" src="images/No1-120ml-600x750.png" width="700" height="400" alt="No1 120ml"></a>
-              <div class="card-body">
-                <h4 class="card-title">
+            </div>
+            <!-- /.col-lg-3 -->
+
+            <div class="col-lg-9">
+
+                <div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner" role="listbox">
+                        <div class="carousel-item active">
+                            <img class="d-block img-fluid" src="images/D_yJ8EJWkAM17Ua.jpeg" width="900" height="350" alt="First slide">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block img-fluid" src="images/D_yJ8EJWkAM17Ua.jpeg" width="900" height="350" alt="Second slide">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block img-fluid" src="images/D_yJ8EJWkAM17Ua.jpeg" width="900" height="350" alt="Third slide">
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+
+                <div class="row">
+
+                    <div class="col-lg-4 col-md-6 mb-4">
+                        <div class="card h-100">
+                            <a href="#"><img class="card-img-top" src="images/No1-120ml-600x750.png" width="700" height="400" alt="No1 120ml"></a>
+                            <div class="card-body">
+                                <h4 class="card-title">
                   <a href="#">No1 120ml</a>
                 </h4>
-                <h5>£3.99</h5>
-                <p class="card-text">A delicious mixed berry pop, popular among many brits.</p>
-              </div>
-              <div class="card-footer">
-                <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-              </div>
-            </div>
-          </div>
+                                <h5>£3.99</h5>
+                                <p class="card-text">A delicious mixed berry pop, popular among many brits.</p>
+                            </div>
+                            <div class="card-footer">
+                                <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+                            </div>
+                        </div>
+                    </div>
 
-          <div class="col-lg-4 col-md-6 mb-4">
-            <div class="card h-100">
-              <a href="#"><img class="card-img-top" src="images/No2-120ml-600x750.png" width="700" height="400" alt="No2 120ml"></a>
-              <div class="card-body">
-                <h4 class="card-title">
+                    <div class="col-lg-4 col-md-6 mb-4">
+                        <div class="card h-100">
+                            <a href="#"><img class="card-img-top" src="images/No2-120ml-600x750.png" width="700" height="400" alt="No2 120ml"></a>
+                            <div class="card-body">
+                                <h4 class="card-title">
                   <a href="#">No2 120ml</a>
                 </h4>
-                <h5>£3.99</h5>
-                <p class="card-text">Chilled Cherry Candy, the best Cherry Menthol around.</p>
-              </div>
-              <div class="card-footer">
-                <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-              </div>
-            </div>
-          </div>
+                                <h5>£3.99</h5>
+                                <p class="card-text">Chilled Cherry Candy, the best Cherry Menthol around.</p>
+                            </div>
+                            <div class="card-footer">
+                                <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+                            </div>
+                        </div>
+                    </div>
 
-          <div class="col-lg-4 col-md-6 mb-4">
-            <div class="card h-100">
-              <a href="#"><img class="card-img-top" src="images/No3-120ml-600x750.png" width="700" height="400" alt="No3 120ml"></a>
-              <div class="card-body">
-                <h4 class="card-title">
+                    <div class="col-lg-4 col-md-6 mb-4">
+                        <div class="card h-100">
+                            <a href="#"><img class="card-img-top" src="images/No3-120ml-600x750.png" width="700" height="400" alt="No3 120ml"></a>
+                            <div class="card-body">
+                                <h4 class="card-title">
                   <a href="#">No3 120ml</a>
                 </h4>
-                <h5>£3.99</h5>
-                <p class="card-text">Blackcurrants and blackberries with a refreshing cool breeze.</p>
-              </div>
-              <div class="card-footer">
-                <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-              </div>
-            </div>
-          </div>
+                                <h5>£3.99</h5>
+                                <p class="card-text">Blackcurrants and blackberries with a refreshing cool breeze.</p>
+                            </div>
+                            <div class="card-footer">
+                                <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+                            </div>
+                        </div>
+                    </div>
 
-          <div class="col-lg-4 col-md-6 mb-4">
-            <div class="card h-100">
-              <a href="#"><img class="card-img-top" src="images/No4-120ml-600x750.png" width="700" height="400" alt="No4 120ml"></a>
-              <div class="card-body">
-                <h4 class="card-title">
+                    <div class="col-lg-4 col-md-6 mb-4">
+                        <div class="card h-100">
+                            <a href="#"><img class="card-img-top" src="images/No4-120ml-600x750.png" width="700" height="400" alt="No4 120ml"></a>
+                            <div class="card-body">
+                                <h4 class="card-title">
                   <a href="#">No4 120ml</a>
                 </h4>
-                <h5>£3.99</h5>
-                <p class="card-text">Mixed berry fruits with a hint of menthol.</p>
-              </div>
-              <div class="card-footer">
-                <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-              </div>
-            </div>
-          </div>
+                                <h5>£3.99</h5>
+                                <p class="card-text">Mixed berry fruits with a hint of menthol.</p>
+                            </div>
+                            <div class="card-footer">
+                                <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+                            </div>
+                        </div>
+                    </div>
 
-          <div class="col-lg-4 col-md-6 mb-4">
-            <div class="card h-100">
-              <a href="#"><img class="card-img-top" src="images/No5-120ml-600x750.png" width="700" height="400" alt="No5 120ml"></a>
-              <div class="card-body">
-                <h4 class="card-title">
+                    <div class="col-lg-4 col-md-6 mb-4">
+                        <div class="card h-100">
+                            <a href="#"><img class="card-img-top" src="images/No5-120ml-600x750.png" width="700" height="400" alt="No5 120ml"></a>
+                            <div class="card-body">
+                                <h4 class="card-title">
                   <a href="#">No5 120ml</a>
                 </h4>
-                <h5>£3.99</h5>
-                <p class="card-text">An icy cold blast with frozen mint.</p>
-              </div>
-              <div class="card-footer">
-                <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-              </div>
-            </div>
-          </div>
+                                <h5>£3.99</h5>
+                                <p class="card-text">An icy cold blast with frozen mint.</p>
+                            </div>
+                            <div class="card-footer">
+                                <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+                            </div>
+                        </div>
+                    </div>
 
-          <div class="col-lg-4 col-md-6 mb-4">
-            <div class="card h-100">
-              <a href="#"><img class="card-img-top" src="images/No6-120ml-600x750.png" width="700" height="400" alt="No6 120ml"></a>
-              <div class="card-body">
-                <h4 class="card-title">
-                  <a href="#">No6 120ml</a>
-                </h4>
-                <h5>£3.99</h5>
-                <p class="card-text">Candied Raspberries with a surprise centre.</p>
-              </div>
-              <div class="card-footer">
-                <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-              </div>
+                    <div class="col-lg-4 col-md-6 mb-4">
+                        <div class="card h-100">
+                            <a href="#"><img class="card-img-top" src="images/No6-120ml-600x750.png" width="700" height="400" alt="No6 120ml"></a>
+                            <div class="card-body">
+                                <h4 class="card-title">
+                                    <a href="#">No6 120ml</a>
+                                </h4>
+                                <h5>£3.99</h5>
+                                <p class="card-text">Candied Raspberries with a surprise centre.</p>
+                            </div>
+                            <div class="card-footer">
+                                <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-
         </div>
-        <!-- /.row -->
-
-      </div>
-      <!-- /.col-lg-9 -->
-
     </div>
-    <!-- /.row -->
 
-  </div>
-  <!-- /.container -->
+    <!-- Footer -->
+    <footer class="py-5 bg-dark">
+        <div class="container">
+            <p class="m-0 text-center text-white">Copyright &copy; <?php echo $global_vars['shop_title']; ?> <?php echo date("Y", time()); ?></p>
+        </div>
+    </footer>
 
-  <!-- Footer -->
-  <footer class="py-5 bg-dark">
-    <div class="container">
-      <p class="m-0 text-center text-white">Copyright &copy; UBLO.Club 2019</p>
-    </div>
-    <!-- /.container -->
-  </footer>
-
-  <!-- Bootstrap core JavaScript -->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
 </body>
 
