ba37329ccb99b00e8abf9b28ee8ac4ba3db33a48
Jamie Whittingham <jamie.whittingham@gmail.com>
2019-12-18 19:58:26 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 
-LOG=/home2/ublo/public_html/ublo.log
+LOG=/home/ublo/public_html/ublo.log
 
-VERSION=$(cat /home2/ublo/public_html/version.txt)
+VERSION=$(cat /home/ublo/public_html/version.txt)
 
 echo "Ublo.Club - Update Script v"$VERSION
 
-# set git repo
-# git remote set-url origin https://github.com/whittinghamj/slistream_cms_production.git
-
 # bash git update script
-cd /home2/ublo/public_html >> $LOG
-git reset --hard -q origin/master >> $LOG
+cd /home/ublo/public_html >> $LOG
 
 git pull >> $LOG
-
-find . -type d -print0 | xargs -0 chmod 0755
-find . -type f -print0 | xargs -0 chmod 0644
 
 echo "Update Complete"
 echo " "
