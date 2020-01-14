6f47a0991a69c314ac59dc3fb3b3b95c1cd6747a
Jamie Whittingham <jamie.whittingham@gmail.com>
2020-01-14 22:29:52 +0000

    Update index.php

--- index.php
+++ index.php
@@ -182,7 +182,32 @@ $all_categories 		= $query->fetchAll(PDO::FETCH_ASSOC);
 												</span>
 											</a>
 											<div class="header-nav-features-dropdown" id="headerTopCartDropdown">
-												Cart is empty
+												<?php if(isset($_SESSION['cart'])){ ?>
+													<ol class="mini-products-list">
+														<li class="item">
+															<a href="#" title="Camera X1000" class="product-image"><img src="img/products/product-1.jpg" alt="Camera X1000"></a>
+															<div class="product-details">
+																<p class="product-name">
+																	<a href="#">Camera X1000 </a>
+																</p>
+																<p class="qty-price">
+																	 1X <span class="price">$890</span>
+																</p>
+																<a href="#" title="Remove This Item" class="btn-remove"><i class="fas fa-times"></i></a>
+															</div>
+														</li>
+													</ol>
+													<div class="totals">
+														<span class="label">Total:</span>
+														<span class="price-total"><span class="price">$890</span></span>
+													</div>
+													<div class="actions">
+														<a class="btn btn-dark" href="#">View Cart</a>
+														<a class="btn btn-primary" href="#">Checkout</a>
+													</div>
+												<?php }else{ ?>
+													Cart is empty.
+												<?php } ?>
 											</div>
 										</div>
 									</div>
