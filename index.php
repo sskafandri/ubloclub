09f539bc57bdde95daf3d7231e0b5a59a4f53218
Jamie Whittingham <jamie.whittingham@gmail.com>
2020-01-14 21:13:06 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1339,7 +1339,7 @@ $all_categories 		= $query->fetchAll(PDO::FETCH_ASSOC);
 						$linked_products[$count]['primary'] 		= $linked_product_raw['primary'];
 						$linked_products[$count]['secondary'] 		= $linked_product_raw['secondary'];
 
-						$query 					= $conn->query("SELECT `id`,`title` FROM `shop_productss` WHERE `id` = '".$linked_product_raw['secondary']."' ");
+						$query 					= $conn->query("SELECT `id`,`title` FROM `shop_products` WHERE `id` = '".$linked_product_raw['secondary']."' ");
 						$linked_product_data 	= $query->fetch(PDO::FETCH_ASSOC);
 						$linked_products[$count]['title']			= stripslashes($linked_product_data['title']);
 
