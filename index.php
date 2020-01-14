4e13a3b73a0b90b9d837a9e1a40a5147dce42059
Jamie Whittingham <jamie.whittingham@gmail.com>
2020-01-14 13:36:35 +0000

    Update index.php

--- index.php
+++ index.php
@@ -73,7 +73,8 @@ $all_categories 		= $query->fetchAll(PDO::FETCH_ASSOC);
 		<link rel="stylesheet" href="css/theme-shop.css">
 
 		<!-- Skin CSS -->
-		<link rel="stylesheet" href="css/skins/default.css"> 
+		<!-- <link rel="stylesheet" href="css/skins/skin-ublo.css"> -->
+		<link rel="stylesheet" href="css/skins/default.css">
 
 		<!-- Theme Custom CSS -->
 		<link rel="stylesheet" href="css/custom.css">
