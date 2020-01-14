39212bef93a3e8bd6ad586c57570f59005aedbef
Jamie Whittingham <jamie.whittingham@gmail.com>
2020-01-14 14:17:18 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1319,7 +1319,7 @@ $all_categories 		= $query->fetchAll(PDO::FETCH_ASSOC);
 				<div role="main" class="main shop py-4">
 					<div class="container">
 						<div class="row">
-							<div class="col-lg-9">
+							<div class="col-lg-12">
 								<div class="row">
 									<div class="col-lg-6">
 										<div class="owl-carousel owl-theme" data-plugin-options="{'items': 1, 'margin': 10}">
@@ -1540,103 +1540,105 @@ $all_categories 		= $query->fetchAll(PDO::FETCH_ASSOC);
 									</div>
 								</div>
 							</div>
-							<div class="col-lg-3">
-								<aside class="sidebar">
-									<form action="page-search-results.html" method="get">
-										<div class="input-group mb-3 pb-1">
-											<input class="form-control text-1" placeholder="Search..." name="s" id="s" type="text">
-											<span class="input-group-append">
-												<button type="submit" class="btn btn-dark text-1 p-2"><i class="fas fa-search m-2"></i></button>
-											</span>
+							<!--
+								<div class="col-lg-3">
+									<aside class="sidebar">
+										<form action="page-search-results.html" method="get">
+											<div class="input-group mb-3 pb-1">
+												<input class="form-control text-1" placeholder="Search..." name="s" id="s" type="text">
+												<span class="input-group-append">
+													<button type="submit" class="btn btn-dark text-1 p-2"><i class="fas fa-search m-2"></i></button>
+												</span>
+											</div>
+										</form>
+										<h5 class="font-weight-bold pt-3">Categories</h5>
+										<ul class="nav nav-list flex-column">
+											<li class="nav-item"><a class="nav-link" href="#">Arts & Crafts</a></li>
+											<li class="nav-item"><a class="nav-link" href="#">Automotive</a></li>
+											<li class="nav-item"><a class="nav-link" href="#">Baby</a></li>
+											<li class="nav-item"><a class="nav-link" href="#">Books</a></li>
+											<li class="nav-item"><a class="nav-link" href="#">Eletronics</a></li>
+											<li class="nav-item"><a class="nav-link" href="#">Women's Fashion</a></li>
+											<li class="nav-item"><a class="nav-link" href="#">Men's Fashion</a></li>
+											<li class="nav-item"><a class="nav-link" href="#">Health & Household</a></li>
+											<li class="nav-item"><a class="nav-link" href="#">Home & Kitchen</a></li>
+											<li class="nav-item"><a class="nav-link" href="#">Military Accessories</a></li>
+											<li class="nav-item"><a class="nav-link" href="#">Movies & Television</a></li>
+											<li class="nav-item"><a class="nav-link" href="#">Sports & Outdoors</a></li>
+											<li class="nav-item"><a class="nav-link" href="#">Tools & Home Improvement</a></li>
+											<li class="nav-item"><a class="nav-link" href="#">Toys & Games</a></li>
+										</ul>
+										<h5 class="font-weight-bold pt-5">Tags</h5>
+										<div class="mb-3 pb-1">
+											<a href="#"><span class="badge badge-dark badge-sm badge-pill text-uppercase px-2 py-1 mr-1">Nike</span></a>
+											<a href="#"><span class="badge badge-dark badge-sm badge-pill text-uppercase px-2 py-1 mr-1">Travel</span></a>
+											<a href="#"><span class="badge badge-dark badge-sm badge-pill text-uppercase px-2 py-1 mr-1">Sport</span></a>
+											<a href="#"><span class="badge badge-dark badge-sm badge-pill text-uppercase px-2 py-1 mr-1">TV</span></a>
+											<a href="#"><span class="badge badge-dark badge-sm badge-pill text-uppercase px-2 py-1 mr-1">Books</span></a>
+											<a href="#"><span class="badge badge-dark badge-sm badge-pill text-uppercase px-2 py-1 mr-1">Tech</span></a>
+											<a href="#"><span class="badge badge-dark badge-sm badge-pill text-uppercase px-2 py-1 mr-1">Adidas</span></a>
+											<a href="#"><span class="badge badge-dark badge-sm badge-pill text-uppercase px-2 py-1 mr-1">Promo</span></a>
+											<a href="#"><span class="badge badge-dark badge-sm badge-pill text-uppercase px-2 py-1 mr-1">Reading</span></a>
+											<a href="#"><span class="badge badge-dark badge-sm badge-pill text-uppercase px-2 py-1 mr-1">Social</span></a>
+											<a href="#"><span class="badge badge-dark badge-sm badge-pill text-uppercase px-2 py-1 mr-1">Books</span></a>
+											<a href="#"><span class="badge badge-dark badge-sm badge-pill text-uppercase px-2 py-1 mr-1">Tech</span></a>
+											<a href="#"><span class="badge badge-dark badge-sm badge-pill text-uppercase px-2 py-1 mr-1">New</span></a>
 										</div>
-									</form>
-									<h5 class="font-weight-bold pt-3">Categories</h5>
-									<ul class="nav nav-list flex-column">
-										<li class="nav-item"><a class="nav-link" href="#">Arts & Crafts</a></li>
-										<li class="nav-item"><a class="nav-link" href="#">Automotive</a></li>
-										<li class="nav-item"><a class="nav-link" href="#">Baby</a></li>
-										<li class="nav-item"><a class="nav-link" href="#">Books</a></li>
-										<li class="nav-item"><a class="nav-link" href="#">Eletronics</a></li>
-										<li class="nav-item"><a class="nav-link" href="#">Women's Fashion</a></li>
-										<li class="nav-item"><a class="nav-link" href="#">Men's Fashion</a></li>
-										<li class="nav-item"><a class="nav-link" href="#">Health & Household</a></li>
-										<li class="nav-item"><a class="nav-link" href="#">Home & Kitchen</a></li>
-										<li class="nav-item"><a class="nav-link" href="#">Military Accessories</a></li>
-										<li class="nav-item"><a class="nav-link" href="#">Movies & Television</a></li>
-										<li class="nav-item"><a class="nav-link" href="#">Sports & Outdoors</a></li>
-										<li class="nav-item"><a class="nav-link" href="#">Tools & Home Improvement</a></li>
-										<li class="nav-item"><a class="nav-link" href="#">Toys & Games</a></li>
-									</ul>
-									<h5 class="font-weight-bold pt-5">Tags</h5>
-									<div class="mb-3 pb-1">
-										<a href="#"><span class="badge badge-dark badge-sm badge-pill text-uppercase px-2 py-1 mr-1">Nike</span></a>
-										<a href="#"><span class="badge badge-dark badge-sm badge-pill text-uppercase px-2 py-1 mr-1">Travel</span></a>
-										<a href="#"><span class="badge badge-dark badge-sm badge-pill text-uppercase px-2 py-1 mr-1">Sport</span></a>
-										<a href="#"><span class="badge badge-dark badge-sm badge-pill text-uppercase px-2 py-1 mr-1">TV</span></a>
-										<a href="#"><span class="badge badge-dark badge-sm badge-pill text-uppercase px-2 py-1 mr-1">Books</span></a>
-										<a href="#"><span class="badge badge-dark badge-sm badge-pill text-uppercase px-2 py-1 mr-1">Tech</span></a>
-										<a href="#"><span class="badge badge-dark badge-sm badge-pill text-uppercase px-2 py-1 mr-1">Adidas</span></a>
-										<a href="#"><span class="badge badge-dark badge-sm badge-pill text-uppercase px-2 py-1 mr-1">Promo</span></a>
-										<a href="#"><span class="badge badge-dark badge-sm badge-pill text-uppercase px-2 py-1 mr-1">Reading</span></a>
-										<a href="#"><span class="badge badge-dark badge-sm badge-pill text-uppercase px-2 py-1 mr-1">Social</span></a>
-										<a href="#"><span class="badge badge-dark badge-sm badge-pill text-uppercase px-2 py-1 mr-1">Books</span></a>
-										<a href="#"><span class="badge badge-dark badge-sm badge-pill text-uppercase px-2 py-1 mr-1">Tech</span></a>
-										<a href="#"><span class="badge badge-dark badge-sm badge-pill text-uppercase px-2 py-1 mr-1">New</span></a>
-									</div>
-									<div class="row mb-5">
-										<div class="col">
-											<h5 class="font-weight-bold pt-5">Top Rated Products</h5>
-											<ul class="simple-post-list">
-												<li>
-													<div class="post-image">
-														<div class="d-block">
-															<a href="shop-product-sidebar-left.html">
-																<img alt="" width="60" height="60" class="img-fluid" src="img/products/product-grey-1.jpg">
-															</a>
+										<div class="row mb-5">
+											<div class="col">
+												<h5 class="font-weight-bold pt-5">Top Rated Products</h5>
+												<ul class="simple-post-list">
+													<li>
+														<div class="post-image">
+															<div class="d-block">
+																<a href="shop-product-sidebar-left.html">
+																	<img alt="" width="60" height="60" class="img-fluid" src="img/products/product-grey-1.jpg">
+																</a>
+															</div>
 														</div>
-													</div>
-													<div class="post-info">
-														<a href="shop-product-sidebar-left.html">Photo Camera</a>
-														<div class="post-meta text-dark font-weight-semibold">
-															$299
+														<div class="post-info">
+															<a href="shop-product-sidebar-left.html">Photo Camera</a>
+															<div class="post-meta text-dark font-weight-semibold">
+																$299
+															</div>
 														</div>
-													</div>
-												</li>
-												<li>
-													<div class="post-image">
-														<div class="d-block">
-															<a href="shop-product-sidebar-left.html">
-																<img alt="" width="60" height="60" class="img-fluid" src="img/products/product-grey-4.jpg">
-															</a>
+													</li>
+													<li>
+														<div class="post-image">
+															<div class="d-block">
+																<a href="shop-product-sidebar-left.html">
+																	<img alt="" width="60" height="60" class="img-fluid" src="img/products/product-grey-4.jpg">
+																</a>
+															</div>
 														</div>
-													</div>
-													<div class="post-info">
-														<a href="shop-product-sidebar-left.html">Luxury bag</a>
-														<div class="post-meta text-dark font-weight-semibold">
-															$199
+														<div class="post-info">
+															<a href="shop-product-sidebar-left.html">Luxury bag</a>
+															<div class="post-meta text-dark font-weight-semibold">
+																$199
+															</div>
 														</div>
-													</div>
-												</li>
-												<li>
-													<div class="post-image">
-														<div class="d-block">
-															<a href="shop-product-sidebar-left.html">
-																<img alt="" width="60" height="60" class="img-fluid" src="img/products/product-grey-8.jpg">
-															</a>
+													</li>
+													<li>
+														<div class="post-image">
+															<div class="d-block">
+																<a href="shop-product-sidebar-left.html">
+																	<img alt="" width="60" height="60" class="img-fluid" src="img/products/product-grey-8.jpg">
+																</a>
+															</div>
 														</div>
-													</div>
-													<div class="post-info">
-														<a href="shop-product-sidebar-left.html">Military Rucksack</a>
-														<div class="post-meta text-dark font-weight-semibold">
-															$49
+														<div class="post-info">
+															<a href="shop-product-sidebar-left.html">Military Rucksack</a>
+															<div class="post-meta text-dark font-weight-semibold">
+																$49
+															</div>
 														</div>
-													</div>
-												</li>
-											</ul>
+													</li>
+												</ul>
+											</div>
 										</div>
-									</div>
-								</aside>
-							</div>
+									</aside>
+								</div>
+							-->
 						</div>
 					</div>
 				</div>
