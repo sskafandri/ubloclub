6337db7e0bc4cfdedd1985c52d0ea1a6e49c3e18
Jamie Whittingham <jamie.whittingham@gmail.com>
2020-01-16 04:31:44 +0000

    Update actions.php

--- actions.php
+++ actions.php
@@ -449,13 +449,13 @@ function checkout(){
 		
 		$hash 				= sha1($email.$timestamp.$autoauthkey);
 		
-		$url 				= $whmcsurl."?email=$email&timestamp=".$timestamp."&hash=".$hash."&goto=".urlencode($goto);
+		$url 				= $whmcsurl."?email=$email&timestamp=".$timestamp."&hash=".$hash."&goto=".$goto;
 
 		// empty_cart();
 
-		echo "URL: ".$url;
+		// echo "URL: ".$url;
 
-		// go($url);
+		go(urlencode($url);
 	}else{
 		status_message('danger',$results['message'].".");
 		go($_SERVER['HTTP_REFERER']);
