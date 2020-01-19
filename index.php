f018727ec1c0549b906b6520f7dc8f72e0a3c218
Jamie Whittingham <jamie.whittingham@gmail.com>
2020-01-19 19:02:29 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1390,31 +1390,22 @@ $cart_items 						= $query->fetchAll(PDO::FETCH_ASSOC);
 Ublo Club complies with the General Data Protection Regulation (GDPR) 2018<br>
 <br>
 Your privacy is important to us. Read this policy to learn about the personal data that we collect from you and how we use it. We encourage you to read the policy and get in touch if you have any queries.<br>
-<br>
 This Privacy Policy describes how your personal information is collected, used, and shared when you visit or make a purchase from https://ublo.club (the “Site”).<br>
 <br>
 
 <strong>PERSONAL INFORMATION WE COLLECT</strong><br>
 When you visit the Site, we automatically collect certain information about your device, including information about your web browser, IP address, time zone, and some of the cookies that are installed on your device. Additionally, as you browse the Site, we collect information about the individual web pages or products that you view, what websites or search terms referred you to the Site, and information about how you interact with the Site. We refer to this automatically collected information as “Device Information.”<br>
-<br>
 We collect Device Information using the following technologies:<br>
-<br>
 – “Cookies” are data files that are placed on your device or computer and often include an anonymous unique identifier. For more information about cookies, and how to disable cookies, visit http://www.allaboutcookies.org.<br>
-<br>
 – “Log files” track actions occurring on the Site, and collect data including your IP address, browser type, Internet service provider, referring/exit pages, and date/time stamps.<br>
-<br>
 – “Web beacons,” “tags,” and “pixels” are electronic files used to record information about how you browse the Site.<br>
-<br>
 Additionally when you make a purchase or attempt to make a purchase through the Site, we collect certain information from you, including your name, billing address, shipping address, payment information (including credit or debit card numbers, email address, and phone number). We refer to this information as “Order Information.”<br>
-<br>
 When we talk about “Personal Information” in this Privacy Policy, we are talking both about Device Information and Order Information.<br>
 <br>
 
 <strong>HOW DO WE USE YOUR PERSONAL INFORMATION?</strong><br>
 We use the Order Information that we collect generally to fulfil any orders placed through the Site (including processing your payment information, arranging for shipping, and providing you with invoices and/or order confirmations). Additionally, we use this Order Information to:<br>
-<br>
 Communicate with you;<br>
-<br>
 Screen our orders for potential risk or fraud; and<br>
 When in line with the preferences you have shared with us, provide you with information or advertising relating to our products or services.<br>
 We use the Device Information that we collect to help us screen for potential risk and fraud (in particular, your IP address), and more generally to improve and optimise our Site (for example, by generating analytics about how our customers browse and interact with the Site, and to assess the success of our marketing and advertising campaigns).<br>
@@ -1454,7 +1445,6 @@ Specific terms relating to accessing our website and the purchase of our product
 
 <strong>CONTACT US</strong><br>
 For more information about our privacy practices, if you have questions, or if you would like to make a complaint, please contact us via the contact form on this website or by mail using the details provided below:<br>
-<br>
 Ublo Club Ltd, 20-22 Wenlock Road, London, England, N1 7GU
 
 
