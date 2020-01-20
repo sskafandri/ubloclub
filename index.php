2c214778d243090b07628234bb96f80c3b09c142
Jamie Whittingham <jamie.whittingham@gmail.com>
2020-01-20 15:18:48 +0000

    Update index.php

--- index.php
+++ index.php
@@ -103,33 +103,18 @@ $cart_items 						= $query->fetchAll(PDO::FETCH_ASSOC);
 										</nav>
 									</div>
 								</div>
-								<!--
-									<div class="header-column justify-content-end">
-										<div class="header-row">
-											<nav class="header-nav-top">
-												<ul class="nav nav-pills">
-													<li class="nav-item nav-item-anim-icon d-none d-md-block">
-														<a class="nav-link pl-0" href="?c=about_us"><i class="fas fa-angle-right"></i> About Us</a>
-													</li>
-													<li class="nav-item nav-item-anim-icon d-none d-md-block">
-														<a class="nav-link" href="?c=contact_us"><i class="fas fa-angle-right"></i> Contact Us</a>
-													</li>
-													<li class="nav-item dropdown nav-item-left-border d-none d-sm-block">
-														<a class="nav-link" href="#" role="button" id="dropdownLanguage" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-															<img src="img/blank.gif" class="flag flag-us" alt="English" /> English
-															<i class="fas fa-angle-down"></i>
-														</a>
-														<div class="dropdown-menu" aria-labelledby="dropdownLanguage">
-															<a class="dropdown-item" href="#"><img src="img/blank.gif" class="flag flag-us" alt="English" /> English</a>
-															<a class="dropdown-item" href="#"><img src="img/blank.gif" class="flag flag-es" alt="English" /> Español</a>
-															<a class="dropdown-item" href="#"><img src="img/blank.gif" class="flag flag-fr" alt="English" /> Française</a>
-														</div>
-													</li>
-												</ul>
-											</nav>
-										</div>
+								
+								<div class="header-column justify-content-end">
+									<div class="header-row">
+										<nav class="header-nav-top">
+											<ul class="nav nav-pills">
+												<li class="nav-item nav-item-anim-icon d-none d-md-block">
+													Premium Quality Vape Juice British Made and Certified
+												</li>
+											</ul>
+										</nav>
 									</div>
-								-->
+								</div>
 							</div>
 						</div>
 					</div>
