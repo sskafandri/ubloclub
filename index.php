bb4b04a5ea1432bff8f23aea986b53edad52c6c1
Jamie Whittingham <jamie.whittingham@gmail.com>
2020-01-21 11:20:03 +0000

    Update index.php

--- index.php
+++ index.php
@@ -430,7 +430,7 @@ $cart_items 						= $query->fetchAll(PDO::FETCH_ASSOC);
 						$count++;
 					}
 
-					$linked_products = array_sort_by_column($linked_products, 'order');
+					array_sort_by_column($linked_products, 'order');
 				?>
 
 				<div role="main" class="main shop py-4">
