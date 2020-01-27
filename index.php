9800d1530ddde0b30d795ef8f9b450e64311d886
Jamie Whittingham <jamie.whittingham@gmail.com>
2020-01-27 16:42:18 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1440,44 +1440,6 @@ $cart_items 						= $query->fetchAll(PDO::FETCH_ASSOC);
 			<?php } ?>
 
 			<footer id="footer">
-				<div class="container my-4 py-2">
-					<div class="row py-4">
-						<!--
-							<div class="col-lg-4 text-center text-lg-left">
-								<h5 class="text-4 text-color-light mb-3">SUBSCRIBE NEWSLETTER</h5>
-								<p class="text-3 mb-0">Get all the latest informaton on Events, Sales and Offfers.</p>
-								<p class="text-3 mb-0">Sign up for newsletter today.</p>
-							</div>
-							<div class="col-lg-5 text-center text-lg-left px-4 mt-1 mt-lg-3">
-								<div class="pt-1 pt-lg-3 mt-1">
-									<div class="alert alert-success d-none" id="newsletterSuccess">
-										<strong>Success!</strong> You've been added to our email list.
-									</div>
-									<div class="alert alert-danger d-none" id="newsletterError"></div>
-									<form id="newsletterForm" action="php/newsletter-subscribe.php" method="POST" class="mw-100">
-										<div class="input-group input-group-rounded">
-											<input class="form-control form-control-sm bg-light px-4 text-3" placeholder="Email Address..." name="newsletterEmail" id="newsletterEmail" type="text">
-											<span class="input-group-append">
-												<button class="btn btn-primary  text-color-light text-2 py-3 px-4" type="submit"><strong>SUBSCRIBE!</strong></button>
-											</span>
-										</div>
-									</form>
-								</div>
-							</div>
-						-->
-						<div class="col-lg-3 text-center text-lg-left">
-							<div class="pt-3 mt-1">
-								<!--
-									<ul class="footer-social-icons social-icons social-icons-clean social-icons-big social-icons-opacity-light social-icons-icon-light mt-0 mt-lg-3">
-										<li class="social-icons-facebook"><a href="https://www.facebook.com/" target="_blank" title="Facebook"><i class="fab fa-facebook-f"></i></a></li>
-										<li class="social-icons-twitter"><a href="https://www.twitter.com/" target="_blank" title="Twitter"><i class="fab fa-twitter"></i></a></li>
-										<li class="social-icons-linkedin"><a href="https://www.linkedin.com/" target="_blank" title="Linkedin"><i class="fab fa-linkedin-in"></i></a></li>
-									</ul>
-								-->
-							</div>
-						</div>
-					</div>
-				</div>
 				<div class="container mt-4 pt-2 pb-5 footer-top-light-border">
 					<div class="row py-4">
 						<div class="col-md-3 text-center text-md-left">
