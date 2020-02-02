addd7dbfe75e534ea056b089f529b593864caf67
Jamie Whittingham <jamie.whittingham@gmail.com>
2020-02-02 20:49:25 +0000

    added google stats code

--- index.php
+++ index.php
@@ -1636,17 +1636,16 @@ if(isset($_SESSION['mlm_affiliate']) && !empty($_SESSION['mlm_affiliate'])){
 		<!-- Theme Initialization Files -->
 		<script src="js/theme.init.js"></script>
 
-		<!-- Google Analytics: Change UA-XXXXX-X to be your site's ID. Go to https://www.google.com/analytics/ for more information.
+		<!-- Google Analytics: Change UA-XXXXX-X to be your site's ID. Go to https://www.google.com/analytics/ for more information. -->
 		<script>
 			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 		
-			ga('create', 'UA-12345678-1', 'auto');
+			ga('create', 'UA-157507971-1', 'auto');
 			ga('send', 'pageview');
 		</script>
-		-->
 
 		<!-- Basic age verification -->
 		<script src="https://cdn.jsdelivr.net/jquery.cookie/1.4.1/jquery.cookie.min.js"></script>
