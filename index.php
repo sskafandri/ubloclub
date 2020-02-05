fd0aedeca4aba4752d8e8f310baeb583ebceb7b1
Jamie Whittingham <jamie.whittingham@gmail.com>
2020-02-05 18:55:20 +0000

    added GeoIP locks

--- index.php
+++ index.php
@@ -17,6 +17,19 @@ if(!isset($_SERVER["HTTPS"]) || $_SERVER["HTTPS"] != "on")
     exit;
 }
 
+//Get user IP address
+$ip 				= $_SERVER['REMOTE_ADDR'];
+//Using the API to get information about this IP
+$details 			= json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=$ip"));
+//Using the geoplugin to get the continent for this IP
+$continent 			= $details->geoplugin_continentCode;
+//And for the country
+$country 			= $details->geoplugin_countryCode;
+//If continent is Europe
+if( $country == "UK" ) {
+	die( "This website is only accessable to customers inside the UK." );   
+}
+
 // includes
 include('inc/db.php');
 include('inc/functions.php');
